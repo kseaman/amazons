4d6f82ad33b9b9d8e2f451e1e5ba3bf50e3dd456
Aditya Jain <aditjain13@gmail.com>
2017-02-24 02:49:42 -0800

    Setup
    - Two Player Game
    - Modified onLogin() func to show game rooms and what we are connecting
    to.

---
+++ TwoPlayerGame.java
@@ -0,0 +1,8 @@
+package ygraphs.ai.smart_fox.games;
+
+public class TwoPlayerGame {
+	public static void main(String[] args) {
+		Amazon game = new Amazon("player1", "pw");
+		Amazon game2 = new Amazon("player2", "pw");
+	}
+}
